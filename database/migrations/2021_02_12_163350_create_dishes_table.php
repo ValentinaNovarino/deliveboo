334783cc9f03e66ce36d9d4dc635ef0f6cebd483
RoccoChiarella <rocco.chiarella.rc@gmail.com>
2021-02-12 17:55:29 +0100

    Model e migration delle varie tabelle

---
+++ database/migrations/2021_02_12_163350_create_dishes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 6,2);
+            $table->text('description');
+            $table->boolean('visible')->default(true);
+            $table->string('cover');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
