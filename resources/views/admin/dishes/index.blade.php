9494b0ddd4407e2ff3daaaad2252cc6e2d4e0723
Ale <alessandrochb@gmail.com>
2021-02-16 22:29:04 +0100

    sistemata select che mostra il menu di ogni singolo ristorante

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -21,83 +21,72 @@
                     <tbody>
                         @if (count($restaurants) > 1)
                             <h3>seleziona il tuo ristorante</h3>
-                            {{-- <select v-model="selected" class="" name="">
-                                <option value=""></option>
-                                <option value=""></option>
-                            </select> --}}
-
-                            <select @change="onChange(this.value)" v-model="selectedValue" class="" name="">
+                            <select @change="onChange(this.value)" v-model="selectedValue">
                                 <option value="">seleziona ristorante</option>
                                 @foreach ($restaurants as $restaurant)
-                                    <option @click="showDishes()" :value="{{$restaurant->id}}">{{ $restaurant->name }}</option>
+                                    <option value="{{$restaurant->id}}">{{ $restaurant->name }}</option>
                                 @endforeach
                             </select>
-
-                            <div v-if="visible" class="prova">
-                                ciao
-                            </div>
                             {{-- @foreach ($dishes as $dish)
-                                @foreach ($dish as $item)
-                                    @if ($restaurant->id == 1)
-                                        <tr>
-                                            <td>{{ $item->id }}</td>
-                                            <td>{{ $item->name }}</td>
-                                            <td>{{ $item->slug }}</td>
-                                            <td>
-                                                <a href="{{ route('admin.dishes.show', ['dish'=> $item->slug ]) }}">
-                                                    <button class="btn btn-primary">Show</button>
-                                                </a>
-                                                <a href="{{ route('admin.dishes.edit', ['dish'=>$item->slug ]) }}">
-                                                    <button class="btn btn-info">Modify</button>
-                                                </a>
-                                                <form class="d-inline"
-                                                action="{{ route('admin.dishes.destroy', ['dish'=>$item->slug]) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
+                                @foreach ($dish as $item) --}}
+                                    <tr v-for="dish in dishes" v-if="visible && dish.restaurant_id == selectedValue">
+                                        <td>@{{ dish.id }}</td>
+                                        <td>@{{ dish.name }}</td>
+                                        <td>@{{ dish.slug }}</td>
+                                        {{-- <td>
+                                            <a href="{{ route('admin.dishes.show', ['dish'=> $item->slug ]) }}">
+                                                <button class="btn btn-primary">Show</button>
+                                            </a>
+                                            <a href="{{ route('admin.dishes.edit', ['dish'=>$item->slug ]) }}">
+                                                <button class="btn btn-info">Modify</button>
+                                            </a>
+                                            <form class="d-inline"
+                                            action="{{ route('admin.dishes.destroy', ['dish'=>$item->slug]) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
                                                 <button class="btn btn-danger" type="submit">Delete</button>
                                             </form>
-                                        </td>
+                                        </td> --}}
                                     </tr>
-                                @endif
-                            @endforeach
-                        @endforeach --}}
-                    @endif
-                </tbody>
-            </table>
+                                {{-- @endforeach
+                            @endforeach --}}
+                        @endif
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 </div>
 <script type="text/javascript">
 var app = new Vue ({
     el: '#root',
     data: {
-        saluto: 'ciao',
         selectedValue: '',
-        // selected: '',
-        valore: '',
-        visible: false
+        changedValue: '',
+        visible: false,
+        dishes: [],
+
     },
     methods: {
         onChange(value) {
+            this.visible = false;
+
             console.log(this.selectedValue);
-            // this.valore = this.selectedValue;
-            // console.log(this.valore);
 
             axios.post('/api/dishes')
             .then((element) => {
                 // this.dishes = response.data.dishes;
                 console.log(element.data.response);
+                console.log(element.data);
+                this.dishes = element.data.response;
                 for (var i = 0; i < element.data.response.length; i++) {
                     console.log(element.data.response[i].restaurant_id);
-                    this.valore = element.data.response[i].restaurant_id;
+                    this.changedValue = element.data.response[i].restaurant_id;
+                    if (this.selectedValue == this.changedValue) {
+                        this.visible = true;
+                    }
                 }
             })
-        },
-        showDishes() {
-            if (this.selectedValue == this.valore) {
-                this.visible = true;
-            }
         },
     },
     mounted() {
