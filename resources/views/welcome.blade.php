a1b897b19a5ff4dfe7bb6d71490015cc666bcba4
Ivan Mincione <ivan.mincione92@outlook.it>
2021-02-17 20:20:47 +0100

    Aggiunto dropdown cart

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 
 @section('content')
 
+
+
 <div class="wrap">
     {{-- <div class="header-top">
         <div class="container">
@@ -33,8 +35,78 @@
             </div> --}}
         {{-- </div>
     </div> --}}
+
+
+
     <div class="header-bottom">
+        {{-- TEST DROPDOWN CART  --}}
+        <div class="section-dropcart">
+            <button type="button" class="btn-deliveroo " data-toggle="dropdown">
+                @if (!session()->get('cart'))
+                    <i class="fa fa-shopping-cart" aria-hidden="true"></i> Carrello <span class="badge badge-pill badge-danger">0 €</span>
+                @else
+                    @php
+                        $elementi = session()->get('cart');
+                        $totale = 0;
+                    @endphp
+                        {{-- {{ dd($elementi) }} --}}
+                        @foreach ($elementi as $element)
+                            {{-- {{$element['name'] }}
+                            {{$element['quantity'] }}
+                            {{$element['price'] }} --}}
+                            @php
+                                $subtotale = $element['price'] * $element['quantity'];
+                                $totale = $subtotale + $totale;
+                            @endphp
+
+                        @endforeach
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i> Carrello <span class="badge badge-pill badge-danger">{{ $totale }} €</span>
+                    @endif
+
+                {{-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart <span class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span> --}}
+            </button>
+
+            <div class="dropdown-menu">
+                <div class="row total-header-section">
+                    <div class="col-lg-6 col-sm-6 col-6">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i> <span class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
+                    </div>
+
+                    <?php $total = 0 ?>
+                    @foreach((array) session('cart') as $id => $details)
+                        <?php $total += $details['price'] * $details['quantity'] ?>
+                    @endforeach
+
+                    <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
+                        <p>Totale: <span class="text-info">$ {{ $total }}</span></p>
+                    </div>
+                </div>
+
+                @if(session('cart'))
+                    @foreach((array) session('cart') as $id => $details)
+                        <div class="row cart-detail">
+                            <div class="col-lg-4 col-sm-4 col-4 cart-detail-img">
+                                <img src="{{ $details['photo'] }}" />
+                            </div>
+                            <div class="col-lg-8 col-sm-8 col-8 cart-detail-product">
+                                <p>{{ $details['name'] }}</p>
+                                <span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> Quantità:{{ $details['quantity'] }}</span>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 col-12 text-center checkout p-5">
+                        <a href="{{ url('cart') }}" class="btn btn-deliveroo ">Vai al carrello</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- FINE TEST DROP DOWN CART --}}
         <div class="container">
+
             {{-- <div class="titleHeader">
                 <h1>I piatti che ami, a domicilio.</h1>
             </div> --}}
