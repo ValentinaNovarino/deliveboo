d1fe22a51a7d300fd1b158b412ada6e1a73bd7d1
Rocco Chiarella <rocco.chiarella.rc@gmail.com>
2021-02-17 18:53:22 +0100

    Cancellate scritte brutte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,14 +27,10 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-color-primary">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-<<<<<<< HEAD
                     {{-- {{ config('app.name', 'Laravel') }} --}}
                     <div class="logo">
                         <img class="logo-header"src="{{'../img/logo-bianco.jpg'}}" alt="">
                     </div>
-=======
-                    Deliveroo
->>>>>>> main
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
