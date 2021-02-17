68538832a206a5e39564f55d545914d5da4d4f16
Ale <alessandrochb@gmail.com>
2021-02-17 12:42:41 +0100

    creato controlli in login se l'utente non ha ristoranti

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -8,39 +8,41 @@
             <a href="{{ route('admin.dishes.create') }}">
                 <button type="button" class="btn btn-info">Aggiungi nuovo piatto</button>
             </a>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Slug</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($dishes as $dish)
+            @if (count($dishes) > 0)
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>{{ $dish->id }}</td>
-                            <td>{{ $dish->name }}</td>
-                            <td>{{ $dish->slug }}</td>
-                            <td>
-                                <a href="{{ route('admin.dishes.show', ['dish'=> $dish->slug ]) }}">
-                                    <button class="btn btn-primary">Show</button>
-                                </a>
-                                <a href="{{ route('admin.dishes.edit', ['dish'=>$dish->slug ]) }}">
-                                    <button class="btn btn-info">Modify</button>
-                                </a>
-                                <form class="d-inline"
-                                action="{{ route('admin.dishes.destroy', ['dish'=>$dish->slug]) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger" type="submit">Delete</button>
+                            <th scope="col">ID</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($dishes as $dish)
+                            <tr>
+                                <td>{{ $dish->id }}</td>
+                                <td>{{ $dish->name }}</td>
+                                <td>{{ $dish->slug }}</td>
+                                <td>
+                                    <a href="{{ route('admin.dishes.show', ['dish'=> $dish->slug ]) }}">
+                                        <button class="btn btn-primary">Show</button>
+                                    </a>
+                                    <a href="{{ route('admin.dishes.edit', ['dish'=>$dish->slug ]) }}">
+                                        <button class="btn btn-info">Modify</button>
+                                    </a>
+                                    <form class="d-inline"
+                                    action="{{ route('admin.dishes.destroy', ['dish'=>$dish->slug]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">Delete</button>
                                 </form>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+            @endif
         </div>
     </div>
 </div>
