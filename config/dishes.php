2c5a8b631d7c4f2a4434716c4592b1e0075105c0
Alba De biase <debiasealba@gmail.com>
2021-03-01 15:29:10 +0100

    database dishes foto e menu pizze

---
+++ config/dishes.php
@@ -0,0 +1,98 @@
+<?php
+
+return [
+    [
+        'name' => 'Pizza bianca',
+        'price' => '8,00',
+        'description' => 'Fior di latte di Agerola',
+        'cover' => 'restaurantsCover/bianca.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pizza rossa',
+        'price' => '7,00',
+        'description' => 'San Marzano e origano',
+        'cover' => 'restaurantsCover/rossa.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pizza Marinara',
+        'price' => '7,50',
+        'description' => 'Salsa di pomodoro San Marzano DOP, origano selvatico, aglio rosso, basilico fresco, olio EVO Pregio DOP',
+        'cover' => 'restaurantsCover/marinara.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pizza Datterino',
+        'price' => '9,00',
+        'description' => 'Pomodorini gialli Giagiù del Vesuvio, fior di latte di Agerola, fior di ricotta di Agerola, basilico fresco e olio evo Pregio DOP',
+        'cover' => 'restaurantsCover/datterini.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Calzone Napoletano',
+        'price' => '7,00',
+        'description' => 'Salame crespone BIO, fior di ricotta e provola affumicata di Agerola, pepe nero, basilico fresco',
+        'cover' => 'restaurantsCover/calzone.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pizza Diavola',
+        'price' => '8,50',
+        'description' => 'Salsa piccante all\'amatriciana, cipolla rossa, pecorino, basilico, olio Pregio, mozzarella di bufala, prosciutto crudo croccante.',
+        'cover' => 'restaurantsCover/diavola.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pizza Lucana',
+        'price' => '8',
+        'description' => 'Baccalà mantecato, pomodorini gialli del Vesuvio, fiordilatte d\'Agerola, polvere di olive nere di Caiazzo, chips di peperone dolce Sciuscillone, zest di limone di Sorrento, basilico fresco, olio evo Incluso',
+        'cover' => 'restaurantsCover/lucana.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pizza 4 formaggi',
+        'price' => '9,50',
+        'description' => 'Gorgonzola di capra Girgentana, mozzarella di bufala Campana DOP, vastedda del Belice DOP, provolone del Monaco DOP, basilico fresco, olio evo Pregio DOP',
+        'cover' => 'restaurantsCover/formaggi.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pizza Margherita',
+        'price' => '6,00',
+        'description' => 'Salsa di San Marzano DOP, fior di latte di Agerola, basilico fresco e olio evo colline salernitane DOP Pregio',
+        'cover' => 'restaurantsCover/margherita.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pizza margherita con bufala',
+        'price' => '9',
+        'description' => 'Salsa di San Marzano DOP, bufala, basilico fresco e olio evo colline salernitane DOP Pregio',
+        'cover' => 'restaurantsCover/bufala.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pizza Brontese',
+        'price' => '10,00',
+        'description' => 'Fiordilatte di Agerola, pesto di pistacchio di Bronte, parmigiano reggiano, scorza di limone, basilico',
+        'cover' => 'restaurantsCover/brontese.jpg',
+        'visible' =>'true'
+    ],
+
+    [
+        'name' => 'Pasta alla genovese',
+        'price' => '8',
+        'description' => 'pasta con pesto di basilico e aglio',
+        'cover' => 'restaurantsCover/genovese.jpg',
+        'visible' =>'true'
+    ],
+
+
+];
