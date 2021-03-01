e1b067807e37aee1d3d5413a023e53bf50aa5634
Alba De biase <debiasealba@gmail.com>
2021-03-01 19:52:05 +0100

    aggiunti piatti cinesi,giapponesi,pasta, poke

--- config/dishes.php
+++ config/dishes.php
@@ -5,7 +5,7 @@ return [
         'name' => 'Pizza bianca',
         'price' => '8,00',
         'description' => 'Fior di latte di Agerola',
-        'cover' => 'restaurantsCover/bianca.jpg',
+        'cover' => 'dishesCover/bianca.jpg',
         'visible' =>'true'
     ],
 
@@ -13,7 +13,7 @@ return [
         'name' => 'Pizza rossa',
         'price' => '7,00',
         'description' => 'San Marzano e origano',
-        'cover' => 'restaurantsCover/rossa.jpg',
+        'cover' => 'dishesCover/rossa.jpg',
         'visible' =>'true'
     ],
 
@@ -21,14 +21,14 @@ return [
         'name' => 'Pizza Marinara',
         'price' => '7,50',
         'description' => 'Salsa di pomodoro San Marzano DOP, origano selvatico, aglio rosso, basilico fresco, olio EVO Pregio DOP',
-        'cover' => 'restaurantsCover/marinara.jpg',
+        'cover' => 'dishesCover/marinara.jpg',
         'visible' =>'true'
     ],
     [
         'name' => 'Pizza Datterino',
         'price' => '9,00',
         'description' => 'Pomodorini gialli Giagiù del Vesuvio, fior di latte di Agerola, fior di ricotta di Agerola, basilico fresco e olio evo Pregio DOP',
-        'cover' => 'restaurantsCover/datterini.jpg',
+        'cover' => 'dishesCover/datterini.jpg',
         'visible' =>'true'
     ],
 
@@ -36,7 +36,7 @@ return [
         'name' => 'Calzone Napoletano',
         'price' => '7,00',
         'description' => 'Salame crespone BIO, fior di ricotta e provola affumicata di Agerola, pepe nero, basilico fresco',
-        'cover' => 'restaurantsCover/calzone.jpg',
+        'cover' => 'dishesCover/calzone.jpg',
         'visible' =>'true'
     ],
 
@@ -44,14 +44,14 @@ return [
         'name' => 'Pizza Diavola',
         'price' => '8,50',
         'description' => 'Salsa piccante all\'amatriciana, cipolla rossa, pecorino, basilico, olio Pregio, mozzarella di bufala, prosciutto crudo croccante.',
-        'cover' => 'restaurantsCover/diavola.jpg',
+        'cover' => 'dishesCover/diavola.jpg',
         'visible' =>'true'
     ],
     [
         'name' => 'Pizza Lucana',
         'price' => '8',
         'description' => 'Baccalà mantecato, pomodorini gialli del Vesuvio, fiordilatte d\'Agerola, polvere di olive nere di Caiazzo, chips di peperone dolce Sciuscillone, zest di limone di Sorrento, basilico fresco, olio evo Incluso',
-        'cover' => 'restaurantsCover/lucana.jpg',
+        'cover' => 'dishesCover/lucana.jpg',
         'visible' =>'true'
     ],
 
@@ -59,7 +59,7 @@ return [
         'name' => 'Pizza 4 formaggi',
         'price' => '9,50',
         'description' => 'Gorgonzola di capra Girgentana, mozzarella di bufala Campana DOP, vastedda del Belice DOP, provolone del Monaco DOP, basilico fresco, olio evo Pregio DOP',
-        'cover' => 'restaurantsCover/formaggi.jpg',
+        'cover' => 'dishesCover/formaggi.jpg',
         'visible' =>'true'
     ],
 
@@ -67,14 +67,14 @@ return [
         'name' => 'Pizza Margherita',
         'price' => '6,00',
         'description' => 'Salsa di San Marzano DOP, fior di latte di Agerola, basilico fresco e olio evo colline salernitane DOP Pregio',
-        'cover' => 'restaurantsCover/margherita.jpg',
+        'cover' => 'dishesCover/margherita.jpg',
         'visible' =>'true'
     ],
     [
         'name' => 'Pizza margherita con bufala',
         'price' => '9',
         'description' => 'Salsa di San Marzano DOP, bufala, basilico fresco e olio evo colline salernitane DOP Pregio',
-        'cover' => 'restaurantsCover/bufala.jpg',
+        'cover' => 'dishesCover/bufala.jpg',
         'visible' =>'true'
     ],
 
@@ -82,15 +82,190 @@ return [
         'name' => 'Pizza Brontese',
         'price' => '10,00',
         'description' => 'Fiordilatte di Agerola, pesto di pistacchio di Bronte, parmigiano reggiano, scorza di limone, basilico',
-        'cover' => 'restaurantsCover/brontese.jpg',
+        'cover' => 'dishesCover/brontese.jpg',
         'visible' =>'true'
     ],
 
     [
+        'name' => 'Pasta Aglio, olio e peperoncino',
+        'price' => '6,00',
+        'description' => 'Aglio, Olio extravergine di oliva, acciughe, prezzemolo, pangrattato e peperoncino',
+        'cover' => 'dishesCover/aglio.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pasta alla Norma',
+        'price' => '7,00',
+        'description' => 'Pomodoro, melanzane, ricotta salata, basilico',
+        'cover' => 'dishesCover/norma.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Tortelli Ricotta e Spinaci',
+        'price' => '10,00',
+        'description' => 'Tortelli di parma con ricotta ed erbette mantecati con burro e parmigiano 24 mesi',
+        'cover' => 'dishesCover/ricotta.jpeg',
+        'visible' =>'true'
+    ],
+    [
         'name' => 'Pasta alla genovese',
-        'price' => '8',
+        'price' => '8,00',
         'description' => 'pasta con pesto di basilico e aglio',
-        'cover' => 'restaurantsCover/genovese.jpg',
+        'cover' => 'dishesCover/genovese.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pasta al ragù di Chianina',
+        'price' => '12,00',
+        'description' => 'Ragù di chianina IGP con passata pomodoro',
+        'cover' => 'dishesCover/chianina.jpeg',
+        'visible' =>'true'
+    ],
+    [
+            'name' => 'Tortelli di zucca',
+        'price' => '13,00',
+        'description' => 'Tortelli di zucca mantecati con burro nocciola salvia e parmigiano',
+        'cover' => 'dishesCover/zucca.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pasta al pomodoro e basilico',
+        'price' => '6,00',
+        'description' => 'Passata di Pomodoro e Basilico',
+        'cover' => 'dishesCover/basilico.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Uramaki di tonno',
+        'price' => '10,00',
+        'description' => 'Tartare di tonno, sesamo, cipollotto, jalapeño esterni con crunch, tobiko e salsa kimchi',
+        'cover' => 'dishesCover/u-tonno.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Sashimi di Salmone',
+        'price' => '5,00',
+        'description' => 'salmone',
+        'cover' => 'dishesCover/sashimi-s.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Sashimi di Ricciola',
+        'price' => '7,00',
+        'description' => 'Ricciola',
+        'cover' => 'dishesCover/sashimi-r.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Uramaki Especial',
+        'price' => '12,00',
+        'description' => 'Gambero in tempura, sesamo, avocado, carpaccio di avocado esterno, tartare di astice gratinata, maionese, spicy cream e salsa teriyaki',
+        'cover' => 'dishesCover/u-special.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Uramaki Pink',
+        'price' => '12,50',
+        'description' => 'pasta con pesto di basilico e aglio',
+        'cover' => 'dishesCover/u-pink.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Temaki',
+        'price' => '8,70',
+        'description' => 'pasta con pesto di basilico e aglio',
+        'cover' => 'dishesCover/temaki.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Nigiri Ricciola',
+        'price' => '11,00',
+        'description' => 'pasta con pesto di basilico e aglio',
+        'cover' => 'dishesCover/nigiri.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pokè Hawaii',
+        'price' => '15,00',
+        'description' => 'Riso bianco, gamberi cotti al vapore, pomodorini, melograno, ananas e edamame',
+        'cover' => 'dishesCover/p-hawaii.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pokè Tuna',
+        'price' => '16,00',
+        'description' => 'Riso bianco, dadolata di tonno, cipolla bianca, cetrioli, mandorle, avocado',
+        'cover' => 'dishesCover/p-tuna.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pokè Salmon',
+        'price' => '15,00',
+        'description' => 'Riso bianco, dadolata di salmone, avocado, cetrioli, edamame',
+        'cover' => 'dishesCover/p-salmon.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Pokè Ahi',
+        'price' => '18,00',
+        'description' => 'Riso bianco, dadolata di salmone, feta greca, avocado, wakame, edamame',
+        'cover' => 'dishesCover/p-ahi.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Edamame',
+        'price' => '5,00',
+        'description' => 'Edamame',
+        'cover' => 'dishesCover/edamame.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Ravioli Cinesi',
+        'price' => '5,00',
+        'description' => 'ravio9li ripieni di carne e verdure',
+        'cover' => 'dishesCover/ravioli-c.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Involtini primavera',
+        'price' => '3,70',
+        'description' => 'involtini di verdure cinesi',
+        'cover' => 'dishesCover/involtini-c.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Riso alla cantonese',
+        'price' => '5,00',
+        'description' => 'Riso con piselli, maiale, uova',
+        'cover' => 'dishesCover/riso-c.jpeg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Panino con Polpo',
+        'price' => '13,50',
+        'description' => 'Pane burger classico, polpo, feta, pomodorini confit, misticanza, semi di zucca, salsa senapata',
+        'cover' => 'dishesCover/polpo-p.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Panino con Salmone',
+        'price' => '12,00',
+        'description' => 'Pane burger integrale, salmone, ricotta aromatizzata al lime con prezzemolo e sale rosa dell’Himalaya, chips di patate croccanti, cipolla caramellata, misticanza e salsa serok',
+        'cover' => 'dishesCover/salmone-p.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Panino con Gamberi',
+        'price' => '14,00',
+        'description' => 'Pane burger classico, gamberi, pomodorini confit, cipolla caramellata, chips di patate croccanti, misticanza, salsa GoodBlue',
+        'cover' => 'dishesCover/gamberi-p.jpg',
+        'visible' =>'true'
+    ],
+    [
+        'name' => 'Panino con Tonno',
+        'price' => '10,00',
+        'description' => 'Pane burger al pomodoro, tonno, stracciatella, pomodorini, misticanza, nocciole, guacamole, sesamo bianco, pesto',
+        'cover' => 'dishesCover/tonno-p.jpg',
         'visible' =>'true'
     ],
 
