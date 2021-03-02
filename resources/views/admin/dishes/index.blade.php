18c8869be47a66beded495f6f1ee5b396f3dafed
Alba De biase <debiasealba@gmail.com>
2021-03-02 18:01:25 +0100

    finita pagina dishes.index

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -5,19 +5,25 @@
     <div class="row justify-content-center">
         <div class="col 12">
             <div class="add-dish d-flex flex-column justify-content-center align-items-center">
-                <h1>Menù del tuo ristorante</h1>
+            <a href="{{ url('/') }}">
+                <img class="logo-admin-dish" src="{{asset('../img/logo.png')}}" alt="">
+            </a>
+                <h1>Componi il menù del tuo ristorante</h1>
                 <a href="{{ route('admin.dishes.create') }}">
-                    <button type="button" class="btn btn-deliveroo new-dish">Aggiungi nuovo piatto</button>
+                    <button class="button9" type="button" class="btn btn-deliveroo new-dish">Aggiungi nuovo piatto
+                        <i class="fas fa-utensils"></i>
+                    </button>
                 </a>
             </div>
             @if (count($dishes) == 0)
-                <h2 class="back-dish-title">Non hai nessun pitto al momento</h2>
+                <h2 class="back-dish-title">Non hai nessun piatto al momento</h2>
             @endif
             @if (count($dishes) > 0)
-                <table class="table">
+                <table class="table_price">
                     <thead>
                         <tr>
-                            <th scope="col">ID</th>
+                            {{-- <th scope="col">ID</th> --}}
+                            <th scope="col">Immagine</th>
                             <th scope="col">Nome</th>
                             <th scope="col">Prezzo</th>
                             <th scope="col">Operazioni</th>
@@ -26,8 +32,9 @@
                     <tbody>
                         @foreach ($dishes as $dish)
                             <tr>
-                                <td>{{ $dish->id }}</td>
-                                <td>{{ $dish->name }}</td>
+                                {{-- <td>{{ $dish->id }}</td> --}}
+                                <td class="td-dish-9"><img src="{{ asset('storage/' . $dish->cover) }}" alt="Cover piatto" ></td>
+                                <td class="capitalize">{{ $dish->name }}</td>
                                 <td>{{ $dish->price }} €</td>
                                 <td>
                                     <a href="{{ route('admin.dishes.show', ['dish'=> $dish->slug ]) }}">
