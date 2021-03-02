77c7f86c5a6a0745e4a7fd309ae411c263be1f4d
ValentinaNovarino <valenovarino@gmail.com>
2021-03-02 18:17:50 +0100

    pagina checkout

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -4,8 +4,25 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                {{-- {{dd(session('final_price'))}} --}}
-                {{-- {{dd(session()->get('cart'))}} --}}
+                <nav class="navbar navbar-expand-lg navbar-light bg-white navbar-restaurants">
+                    <div class="logo-guest-restaurant">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="{{'../img/logo.png'}}" alt="logo deliveroo">
+                        </a>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
                 @if(session('success_message'))
                     <div class="alert alert-success">
                         {{session('success_message')}}
@@ -28,123 +45,132 @@
                         </a>
                     </div>
                 @else
-                    <div class="items-checkout">
-                        @php $finalTotal = 0; @endphp
-                        <h1 class="text-center mb-4">Il tuo ordine</h1>
-                        @foreach ($dishes as $dish)
-                            <div class="card">
-                                <div class="image-container d-inline-block">
-                                    <img src="{{ $dish['cover'] }}" alt="immagine {{ $dish['name'] }}">
+                    {{-- PROVA NUOVA CARD --}}
+                    <div class="checkout">
+                        <div class="items-checkout">
+                            @php $finalTotal = 0; @endphp
+                            <h1 class="mb-4">Il tuo ordine</h1>
+                            <div class="card card-checkout d-flex flex-column">
+                                @foreach ($dishes as $dish)
+                                    <div class="item-container">
+                                        <span><strong>{{ $dish['quantity'] }} x</strong></span>
+                                        <span>{{ $dish['name'] }}</span>
+                                        <div id="info-dish">
+                                            <div class="image-container">
+                                                <img src="{{ asset('storage/'.$dish['cover']) }}" alt="immagine {{ $dish['name'] }}">
+                                            </div>
+                                            <div id="price">
+                                                <p class="price">Prezzo unitario: € {{ $dish['price'] }}</span>
+                                                <p class="price">Totale: € {{ $dish['quantity'] * $dish['price'] }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @php $finalTotal += $dish['quantity'] * $dish['price']; @endphp
+                                @endforeach
+                                <div class="checkout-card-total">
+                                    <h3>Totale ordine: € {{$finalTotal}}</h3>
                                 </div>
-                                <div class="sub-card">
-                                    <p>{{ $dish['name'] }}</p>
-                                    <p>Prezzo unitario: {{ $dish['price'] }} €</p>
-                                    <p class="d-inline-block">Quantità: {{ $dish['quantity'] }}</p>
-                                    <p class="d-inline-block">Totale: {{ $dish['quantity'] * $dish['price'] }} €</p>
+                            </div>
+                            <a href="{{ route('cart') }}">
+                                <button type="button" class="btn btn-lg btn-deliveroo">Torna al carrello</button>
+                            </a>
+                        </div>
+                        <div class="guest-data">
+                            <h1 class="mb-4">Inserisci i tuoi dati</h1>
+                            <form id="form" method="POST" name="formform" action="{{ route('checkout.store') }}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="guestName">Nome</label>
+                                    <input type="text" id="guestName" class="form-control-deliveroo" placeholder="Inserisci il tuo nome" name="guest_name" value="{{ old('guest_name') }}" maxlength="100" required>
+                                    @error('guest_name')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
-                            </div>
-                            @php $finalTotal += $dish['quantity'] * $dish['price']; @endphp
-                        @endforeach
+                                <div class="form-group">
+                                    <label for="guestLastname">Cognome</label>
+                                    <input type="text" id="guestLastname" class="form-control-deliveroo" placeholder="Inserisci il tuo cognome" name="guest_lastname" value="{{ old('guest_lastname') }}" maxlength="100" required>
+                                    @error('guest_lastname')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="guestCity">Città</label>
+                                    <input type="text" id="guestCity" class="form-control-deliveroo" placeholder="Inserisci la città di consegna" name="guest_city" value="{{ old('guest_city') }}" maxlength="100" required>
+                                    @error('guest_city')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="guestAddress">Indirizzo</label>
+                                    <input type="text" id="guestAddress" class="form-control-deliveroo" placeholder="Inserisci l'indirizzo di consegna" name="guest_address" value="{{ old('guest_address') }}" maxlength="100" required>
+                                    @error('guest_address')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label class="checkout-mobile" for="guestMobile">Telefono</label>
+                                    <span class="checkout-mobile">+39</span>
+                                    <input type="text" id="guestMobile" class="form-control-deliveroo checkout-mobile" placeholder="Inserisci il tuo numero" name="guest_mobile" value="{{ old('guest_mobile') }}" maxlength="10" required>
+                                    @error('guest_mobile')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="guestEmail">Email</label>
+                                    <input type="email" id="guestEmail" class="form-control-deliveroo" placeholder="Inserisci la tua email" name="guest_email" value="{{ old('guest_email') }}" maxlength="100" required>
+                                    @error('guest_email')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+
+                                <div id="dropin-wrapper">
+                                    <div id="dropin-container"></div>
+                                </div>
+
+                                {{-- input non visibili --}}
+
+                                <input type="hidden" name="order_price" value="{{ session('order_price')}}">
+                                <input type="hidden" name="delivery_price" value="{{ session('delivery_price')}}">
+                                <input type="hidden" name="discount" value="{{ session('discount')}}">
+                                <input type="hidden" name="final_price" value="{{ session('final_price')}}">
+
+                                {{-- tempo della consegna in secondi --}}
+                                <input type="hidden" name="delivery_time" value="{{ session('delivery_time')}}">
+
+
+
+
+                                {{-- TEST BOTTONE INVIO PAGAMENTO --}}
+                               <button type="submit" class="btn" id="submit-button">
+                                   <div class="container-btn-send-payment">
+                                    <div class="left-side">
+                                     <div class="card-btn-send-payment">
+                                      <div class="card-line"></div>
+                                      <div class="buttons-send-payment"></div>
+                                     </div>
+                                     <div class="post-payment">
+                                      <div class="post-line"></div>
+                                      <div class="screen-btn-payment">
+                                       <div class="dollar-btn-payment">€</div>
+                                      </div>
+                                      <div class="numbers-payment"></div>
+                                      <div class="numbers-line2"></div>
+                                     </div>
+                                    </div>
+                                    <div class="right-side">
+                                     <div class="new-payment">Invia</div>
+
+                                     <i class="fas fa-chevron-right arrow"></i>
+                                    </div>
+                                   </div>
+                               </button>
+
+                               {{-- FINE TEST BOTTONE INVIO PAGAMENTO --}}
+                            </form>
+                        </div>
                     </div>
-                    <div class="checkout-card-total">
-                        <h3>Totale ordine: € {{$finalTotal}}</h3>
-                    </div>
-
-                    <h1 class="text-center mb-4 mt-4">Inserisci i tuoi dati</h1>
-                    <form id="form" method="POST" name="formform" action="{{ route('checkout.store') }}" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group">
-                            <label for="guestName">Nome</label>
-                            <input type="text" id="guestName" class="form-control-deliveroo" placeholder="Inserisci il tuo nome" name="guest_name" value="{{ old('guest_name') }}" maxlength="100" required>
-                            @error('guest_name')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="guestLastname">Cognome</label>
-                            <input type="text" id="guestLastname" class="form-control-deliveroo" placeholder="Inserisci il tuo cognome" name="guest_lastname" value="{{ old('guest_lastname') }}" maxlength="100" required>
-                            @error('guest_lastname')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="guestCity">Città</label>
-                            <input type="text" id="guestCity" class="form-control-deliveroo" placeholder="Inserisci la città di consegna" name="guest_city" value="{{ old('guest_city') }}" maxlength="100" required>
-                            @error('guest_city')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="guestAddress">Indirizzo</label>
-                            <input type="text" id="guestAddress" class="form-control-deliveroo" placeholder="Inserisci l'indirizzo di consegna" name="guest_address" value="{{ old('guest_address') }}" maxlength="100" required>
-                            @error('guest_address')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label class="checkout-mobile" for="guestMobile">Telefono</label>
-                            <span class="checkout-mobile">+39</span>
-                            <input type="text" id="guestMobile" class="form-control-deliveroo checkout-mobile" placeholder="Inserisci il tuo numero" name="guest_mobile" value="{{ old('guest_mobile') }}" maxlength="10" required>
-                            @error('guest_mobile')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="guestEmail">Email</label>
-                            <input type="email" id="guestEmail" class="form-control-deliveroo" placeholder="Inserisci la tua email" name="guest_email" value="{{ old('guest_email') }}" maxlength="100" required>
-                            @error('guest_email')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-
-                        <div id="dropin-wrapper">
-                            <div id="dropin-container"></div>
-                        </div>
-
-                        {{-- input non visibili --}}
-
-                        <input type="hidden" name="order_price" value="{{ session('order_price')}}">
-                        <input type="hidden" name="delivery_price" value="{{ session('delivery_price')}}">
-                        <input type="hidden" name="discount" value="{{ session('discount')}}">
-                        <input type="hidden" name="final_price" value="{{ session('final_price')}}">
-
-                        {{-- tempo della consegna in secondi --}}
-                        <input type="hidden" name="delivery_time" value="{{ session('delivery_time')}}">
-
-
-
-
-                        {{-- TEST BOTTONE INVIO PAGAMENTO --}}
-                       <button type="submit" class="btn" id="submit-button">
-                           <div class="container-btn-send-payment">
-                            <div class="left-side">
-                             <div class="card-btn-send-payment">
-                              <div class="card-line"></div>
-                              <div class="buttons-send-payment"></div>
-                             </div>
-                             <div class="post-payment">
-                              <div class="post-line"></div>
-                              <div class="screen-btn-payment">
-                               <div class="dollar-btn-payment">€</div>
-                              </div>
-                              <div class="numbers-payment"></div>
-                              <div class="numbers-line2"></div>
-                             </div>
-                            </div>
-                            <div class="right-side">
-                             <div class="new-payment">Invia</div>
-
-                             <i class="fas fa-chevron-right arrow"></i>
-                            </div>
-                           </div>
-                       </button>
-
-                       {{-- FINE TEST BOTTONE INVIO PAGAMENTO --}}
-                    </form>
-                    <a href="{{ route('cart') }}">
-                        <button type="button" class="btn btn-lg btn-deliveroo">Torna al carrello</button>
-                    </a>
+                    {{-- FINE PROVA CARD --}}
                 @endif
             </div>
         </div>
