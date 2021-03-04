a4678678807a16494f9fd9ffd1ddb6d4dc955171
Alba De biase <debiasealba@gmail.com>
2021-03-04 13:21:42 +0100

    modifica color register e login homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,11 +31,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link white-n" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link white-n" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
